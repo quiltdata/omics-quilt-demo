4e2a540b5beb69e21dd31d094d9b2f6b7dcd09b5
Dr. Ernie Prabhakar <ernest@quiltdata.io>
2023-12-04 16:08:59 -0800

    chore: configure projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,12 +2,42 @@ import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
+  description: 'Use CDK to create Quilt packages from AWS HealthOmics',
   name: 'omics-quilt-demo',
   projenrcTs: true,
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  deps: [
+    'aws-lambda',
+    '@aws-sdk/client-s3',
+    '@aws-sdk/client-sns',
+    'dotenv',
+    'handlebars',
+    'js-yaml',
+    '@types/js-yaml',
+  ],
+  devDeps: [
+    'eslint',
+  ],
+  gitignore: [
+    '.env*',
+    '.DS_Store',
+    'test/__snapshots__/*',
+  ],
 });
+project.tryFindObjectFile('.github/workflows/build.yml')!.addOverride('jobs.build.env', {
+  CI: 'true',
+  AWS_ACCESS_KEY_ID: '${{ secrets.AWS_ACCESS_KEY_ID }}',
+  AWS_SECRET_ACCESS_KEY: '${{ secrets.AWS_SECRET_ACCESS_KEY }}',
+  AWS_DEFAULT_REGION: '${{ secrets.AWS_DEFAULT_REGION }}',
+  CDK_DEFAULT_ACCOUNT: '${{ secrets.AWS_ACCOUNT_ID }}',
+  CDK_DEFAULT_REGION: '${{ secrets.AWS_DEFAULT_REGION }}',
+  CDK_DEFAULT_EMAIL: '${{ secrets.CDK_DEFAULT_EMAIL }}',
+  QUILT_CATALOG_DOMAIN: '${{ secrets.QUILT_CATALOG_DOMAIN }}',
+});
+// Fix Jest 29 warning about deprecated config in `globals`
+project.jest!.config.transform ??= {};
+project.jest!.config.transform['\\.ts$'] = [
+  'ts-jest',
+  project.jest?.config.globals['ts-jest'],
+];
+delete project.jest!.config.globals['ts-jest'];
 project.synth();
