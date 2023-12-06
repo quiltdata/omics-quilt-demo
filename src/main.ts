68b8a84be7c24b1f9239c1b8cd0847a64171ed12
Dr. Ernie Prabhakar <ernest@quiltdata.io>
2023-12-06 12:47:51 -0800

    lint main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { App } from 'aws-cdk-lib';
 import { OmicsQuiltStack } from './omics-quilt';
 
 function main() {
-    const app = new App();
+  const app = new App();
 
-    new OmicsQuiltStack(app, 'omics-quilt', { env: { region: 'us-west-2' } });
-    // new DiaStack(app, 'vivos-prod', { env: prodEnv });
-    app.synth();
+  new OmicsQuiltStack(app, 'omics-quilt', { env: { region: 'us-west-2' } });
+  // new DiaStack(app, 'vivos-prod', { env: prodEnv });
+  app.synth();
 }
 
 main();
